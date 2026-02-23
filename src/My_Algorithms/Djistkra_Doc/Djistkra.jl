36866f140bda9ead3201f78778daa93492a30087
RogerKTC10 <ntchamroger82@gmail.com>
2026-02-23 19:54:34 +0100

    ...

--- src/My_Algorithms/Djistkra_Doc/Djistkra.jl
+++ src/My_Algorithms/Djistkra_Doc/Djistkra.jl
@@ -13,6 +13,29 @@ function execution_Djisktra(G::Carte_Final_Value_Struct, vdepart, varriver)
     parents = Dict{Tuple{Int, Int}, Tuple{Int, Int}}()
     visiter = Set{vdepart}
     
-    noeud = tas_min()
+    maFile = tas_min()
 
+    maFile[vdepart] = 0.0
+
+    while !isempty(maFile)
+        extrai_min_noeud = dequeue!(maFile)
+        if extrai_min_noeud == varriver
+            break
+        end
+    end
+
+    for voisin in Voisinage(extrai_min_noeud, j)
+        i_visiter, j_visiter = voisin
+
+        valuation_voisin = matriceDjis[i_]
+
+        distance_djistkra = distance_djis[extrai_min_noeud] + valuation_voisin
+        if !haskey(distance_djis, v) || nouvelle_dist < distance_djis[v]
+                distance_djis[v] = nouvelle_dist
+                parents[v] = u
+                
+                # On ajoute ou on met à jour dans ton tas
+                file_priorite[v] = nouvelle_dist
+        end
+    end
 end
