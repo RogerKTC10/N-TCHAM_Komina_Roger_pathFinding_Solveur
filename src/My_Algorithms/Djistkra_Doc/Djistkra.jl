d8f5462f90454df4b41cf4fbf4da884e25f68017
RogerKTC10 <ntchamroger82@gmail.com>
2026-02-24 12:04:38 +0100

    DJISKTRA IMPLEMENTATION TERMINER

--- src/My_Algorithms/Djistkra_Doc/Djistkra.jl
+++ src/My_Algorithms/Djistkra_Doc/Djistkra.jl
@@ -1,8 +1,8 @@
 #using .DataStructure_Min
-using .Struct_Carte
+#using .Struct_Carte
 using DataStructures
 
-function execution_Djisktra(G::Carte_Final_Value_Struct, vdepart, varriver)
+function execution_Djisktra(G::Struct_Carte.Carte_Final_Value_Struct, vdepart, varriver)
     matriceDjis = G.grille_val  #Iici je considère ma matrice comme un graphe dont les sommet sont les cases et les arretes sont les deplacement Nord, Ouest, Est, Sud a faire sur la carte ... 
     h, w = G.height_val, G.width_val
 
